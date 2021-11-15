8fd0729f0c6e6a6e6571b1c08a43928dd7b79dfb
strale15 <karte32114@gmail.com>
2021-11-15 11:51:11 +0100

    Promenjene velicina buttona na toolbar-u

--- src/gui/Toolbar.java
+++ src/gui/Toolbar.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.JToolBar;
 import javax.swing.SwingConstants;
@@ -23,34 +24,50 @@ public class Toolbar extends JToolBar{
 		setBackground(Color.white);
 		setFloatable(false);
 		
+		JPanel toolBarPanel = new JPanel();
+		toolBarPanel.setLayout(new BoxLayout(toolBarPanel, BoxLayout.X_AXIS));
+		add(toolBarPanel);
+		
 		JButton addBtn = new JButton();
 		addBtn.setToolTipText("Dodaj");
+		addBtn.setMaximumSize(new Dimension(50,50));
+		addBtn.setPreferredSize(new Dimension(50,50));
+		addBtn.setBackground(Color.white);
 		addBtn.setIcon(new ImageIcon("icons/new.png"));
-		add(addBtn);
+		toolBarPanel.add(addBtn);
 		
 		JButton editBtn = new JButton();
 		editBtn.setToolTipText("Izmeni");
+		editBtn.setMaximumSize(new Dimension(50,50));
+		editBtn.setPreferredSize(new Dimension(50,50));
+		editBtn.setBackground(Color.white);
 		editBtn.setIcon(new ImageIcon("icons/editPom.png"));
-		add(editBtn);
+		toolBarPanel.add(editBtn);
 		
 		JButton deleteBtn = new JButton();
 		deleteBtn.setToolTipText("Obriši");
+		deleteBtn.setMaximumSize(new Dimension(50,50));
+		deleteBtn.setPreferredSize(new Dimension(50,50));
+		deleteBtn.setBackground(Color.white);
 		deleteBtn.setIcon(new ImageIcon("icons/delete.png"));
-		add(deleteBtn);
+		toolBarPanel.add(deleteBtn);
 		
-		add(Box.createHorizontalGlue());
+		toolBarPanel.add(Box.createHorizontalGlue());
 		
 		JTextField searchBar = new JTextField();
 		searchBar.setToolTipText("Unesi reč za pretragu");
 		searchBar.setMaximumSize(new Dimension(9000,30));
-		add(searchBar);
+		toolBarPanel.add(searchBar);
 		
-		add(Box.createRigidArea(new Dimension(20, 0)));
+		toolBarPanel.add(Box.createRigidArea(new Dimension(10, 0)));
 		
 		JButton searchBtn = new JButton();
 		searchBtn.setToolTipText("Pretraži");
+		searchBtn.setMaximumSize(new Dimension(30,30));
+		searchBtn.setPreferredSize(new Dimension(30,30));
+		searchBtn.setBackground(Color.white);
 		searchBtn.setIcon(new ImageIcon("icons/search.png"));
-		add(searchBtn);
+		toolBarPanel.add(searchBtn);
 		
 		
 	}
