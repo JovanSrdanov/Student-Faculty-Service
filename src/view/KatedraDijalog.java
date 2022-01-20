d1058860694811de415f5134a979ff429fe8ca6b
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-01-20 13:15:04 +0100

    Unos tabele u katedru

--- src/view/KatedraDijalog.java
+++ src/view/KatedraDijalog.java
@@ -1,8 +1,15 @@
 package view;
 
+import java.awt.FlowLayout;
 import java.awt.Frame;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JDialog;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class KatedraDijalog extends JDialog {
 
@@ -10,10 +17,37 @@ public class KatedraDijalog extends JDialog {
 	 * 
 	 */
 	private static final long serialVersionUID = 5756133743805054823L;
+	private Tabela katedreTable;
 
 	public KatedraDijalog(Frame owner, String title, boolean modal) {
 		super(owner, title, modal);
 		setSize(500, 500);
+
+		JPanel centerPanel = new JPanel();
+		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
+
+		katedreTable = new Tabela(new AbstractTableModelKatedra());
+		katedreTable.setAutoCreateRowSorter(true);
+		JScrollPane scrollPaneKatedra = new JScrollPane(katedreTable);
+
+		JPanel btnPan = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		JButton postaviSefaKatedreBtn = new JButton("Postavi sefa katedre");
+		JButton odustaniBtn = new JButton("Odustani");
+
+		odustaniBtn.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+
+			}
+
+		});
+
+		btnPan.add(postaviSefaKatedreBtn);
+		btnPan.add(odustaniBtn);
+		centerPanel.add(btnPan);
+		centerPanel.add(scrollPaneKatedra);
+		this.add(centerPanel);
 
 	}
 }
