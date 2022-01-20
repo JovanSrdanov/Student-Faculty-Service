4c584a144aa812de3c779a06bfeaa8bbcb6d6dd6
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-01-20 23:34:06 +0100

    Brisanje nepotrebnih importova

--- src/model/BazaPredmeta.java
+++ src/model/BazaPredmeta.java
@@ -4,7 +4,6 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
--- src/model/BazaProfesora.java
+++ src/model/BazaProfesora.java
@@ -5,7 +5,6 @@ import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
