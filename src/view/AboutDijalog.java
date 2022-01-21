262814f214787d7197c267d9c604377d2acc6320
JovanSrdanov <93533441+JovanSrdanov@users.noreply.github.com>
2022-01-21 21:06:57 +0100

    Help i about na srpskom

--- src/view/AboutDijalog.java
+++ src/view/AboutDijalog.java
@@ -1,6 +1,7 @@
 package view;
 
 import java.awt.Frame;
+import java.util.Locale;
 
 import javax.swing.BoxLayout;
 import javax.swing.JDialog;
@@ -21,20 +22,35 @@ public class AboutDijalog extends JDialog {
 		JPanel centerPanel = new JPanel();
 		centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
 
-		String s = "<html>Verzija projekta: 1.0<br/>" + "Naziv projekta : Studentska sluzba<br/>" + "<br/>"
-				+ "Aplikacija sluzi da simulira rad studentske sluzbe<br/>"
-				+ "Omoguceni su prikazi, unosi, izmene i brisanja podataka<br/>"
-				+ "Kod svih entiteta je moguce uraditi unos, izmenu i brisanje osim kod katedre <br/>" + "<br/>"
+		String RS = "<html>Verzija projekta: 1.0<br/>"
+				+ "Naziv projekta : Studentska služba<br/>"
 				+ "<br/>"
-				+ "Kod studenta je uradjen prikaz polozenih i nepolozenih predmeta, dovanje i brisanje studenta sa predmeta kao i polaganje i ponistavanje ocene<br/>"
+				+ "Aplikacija služi da simulira rad studentske službe<br/>"
+				+ "Omogućeni su prikazi, unosi, izmene i brisanja podataka<br/>"
+				+ "Kod svih entiteta je moguce uraditi unos, izmenu i brisanje osim kod katedre <br/>"
+				+ "<br/>"
+				+ "<br/>"
+				+ "Kod studenta je uradjen prikaz položenih i nepoloženih predmeta, dodavanje i brisanje studenta sa predmeta kao i polaganje i poništavanje ocene<br/>"
 				+ "Kod profesora je uradjen prikaz predmeta koji predaje, dodavanje predmeta i uklanjanje sa predmeta<br/>"
-				+ "Kod predmeta je omoguceno dodavanje profesora na predmet<br/>"
-				+ "Kod katedre je dozvoljeno samo postavljanje sefa katedre<br/>" + "<br/>"
-				+ "Autor 1: Jovan Srdanov RA 145/2019<br/>" + "Zrenjanin 01.11.2000.<br/>" + "<br/>"
-				+ "Autor 2: Strahinja Erakovic RA 152/2019<br/>" + "Kikinda 22.01.2000.</html>";
-		;
+				+ "Kod predmeta je omogućeno dodavanje profesora na predmet<br/>"
+				+ "Kod katedre je dozvoljeno samo postavljanje šefa katedre<br/>"
+				+ "<br/>"
+				+ "Autor 1: Jovan Srdanov RA 145/2019<br/>"
+				+ "Zrenjanin 01.11.2000.<br/>"
+				+ "<br/>"
+				+ "Autor 2: Strahinja Eraković RA 152/2019<br/>"
+				+ "Kikinda 22.01.2000.</html>";
+		
+		String EN = "en";
+		String kraj;
+				
+		if(!Locale.getDefault().equals(new Locale("sr", "RS")))
+			kraj=EN;
+		else 
+			kraj=RS;
+		
+		JLabel t = new JLabel(kraj,SwingConstants.LEFT);
 
-		JLabel t = new JLabel(s, SwingConstants.LEFT);
 
 		centerPanel.add(t);
 
